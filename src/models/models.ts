8f904afad86a7bef53499c99ac83757edb268bfa
FeuerImBlut <golubovich.as@gmail.com>
2022-08-25 16:49:28 +0300

    implement api, localStoragefunctions; describe data types

---
+++ src/models/models.ts
@@ -0,0 +1,61 @@
+export type Word = {
+  id: string,
+  group: 0,
+  page: 0,
+  word: string,
+  image: string,
+  audio: string,
+  audioMeaning: string,
+  audioExample: string,
+  textMeaning: string,
+  textExample: string,
+  transcription: string,
+  wordTranslate: string,
+  textMeaningTranslate: string,
+  textExampleTranslate: string
+}
+
+export type User = {
+  name: string,
+  email: string,
+  password: string
+}
+
+export type UserResponse = {
+  id: string,
+  name: string,
+  email: string,
+}
+
+export type AuthData = {
+  email: string,
+  password: string
+}
+
+export type AuthResponse = {
+  message: string,
+  name: string,
+  refreshToken: string,
+  token: string,
+  userId: string
+}
+
+export type Statistic = {
+  learnedWords: number,
+  optional?: object
+}
+
+export type Settings = {
+  wordsPerDay: number,
+  optional?: object
+} 
+
+export type UserData = {
+  token: string | null,
+  id: string | null
+}
+
+export type UserWord = {
+  difficulty: string,
+  optional?: object
+}
