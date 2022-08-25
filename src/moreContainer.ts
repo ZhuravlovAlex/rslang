213509902f8f902245fc60e69399090f63484e7c
NastassiaLoginava <nastik.95@bk.ru>
2022-08-25 18:21:56 +0300

    feat: create videoContainer.ts, startContainer.ts, moreContainer.ts, aboutContainer.ts modules

---
+++ src/moreContainer.ts
@@ -0,0 +1,12 @@
+export async function moreButton() {
+	const moreBtn = document.querySelector(".more");
+	const mainContainer = document.querySelector(".main") as HTMLElement;
+	async function more() {
+	  return (mainContainer.innerHTML = `
+        
+
+      `);
+	}
+  
+	moreBtn?.addEventListener("click", more);
+  }
---
+++ src/startContainer.ts
@@ -0,0 +1,12 @@
+export async function startButton() {
+	const startBtn = document.querySelector(".start");
+	const mainContainer = document.querySelector(".main") as HTMLElement;
+	async function start() {
+	  return (mainContainer.innerHTML = `
+        
+
+      `);
+	}
+  
+	startBtn?.addEventListener("click", start);
+  }
