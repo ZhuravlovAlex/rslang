f12446814a3b194a89b27e2fda5abfd74725476a
NastassiaLoginava <nastik.95@bk.ru>
2022-09-02 22:22:32 +0300

    feat: add changes in index.ts, api.ts, global.css modules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
+import { aboutButton } from './containers/aboutContainer';
+import { audioButton } from './containers/audioContainer';
+import { authorizationContainerRender } from './containers/authorizationContainer';
+import { baseContainer } from './containers/baseContainer';
+import { bookContainerRender } from './containers/bookContainer';
+import { moreButton } from './containers/moreContainer';
+import { sprintButton } from './containers/sprintContainer';
+import { startButton } from './containers/startContainer';
+import { statisticButton } from './containers/statisticsContainer';
+import { videoButton } from './containers/videoContainer';
 import './global.css'
-import { start } from './main'
 
+function start() {
+    baseContainer();
+    bookContainerRender();
+    statisticButton();
+    sprintButton();
+    audioButton();
+    moreButton();
+    startButton();
+    videoButton();
+    aboutButton();
+    authorizationContainerRender();
+}
 
 start();
